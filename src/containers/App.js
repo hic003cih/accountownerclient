51db4e67ee1181c911479566f8f055556112b166
Leif <ac676175@gmail.com>
2019-05-13 18:23:39 +0800

    add react-eouter

---
+++ src/containers/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Layout from '../components/Layout/Layout';
+import Home from '../components/Home/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NotFound from '../components/ErrorPages/NotFound/NotFound';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+    );
+  }
+}
+
+export default App;
