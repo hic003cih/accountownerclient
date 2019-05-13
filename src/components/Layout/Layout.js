586e526a2b11ed555271d2ce5441bbc59f6d3735
Leif <ac676175@gmail.com>
2019-05-13 23:57:52 +0800

    Downgrade to Fix bug

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
 import Navigation from '../Navigation/Navigation';
  
 const layout = (props) => {
     return (
-        <Container>
+        <Grid>
             <Row>
                 <Navigation/>
             </Row>
             <main>
                 {props.children}
             </main>
-        </Container>
+        </Grid>
     )
 }
 export default layout;
